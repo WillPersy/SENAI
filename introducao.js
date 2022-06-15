c3b2d09ae71fd2cb77bc5b2ae2320a669f129796
Will Persy <williampersy@hotmail.com>
2022-06-15 13:51:17 -0300

    cont_pecas

---
+++ introducao.js
@@ -0,0 +1,22 @@
+let pesoPeca = 150
+let numeroPeca = 9
+let nomePeca = "Roda"
+
+if(pesoPeca > 100) {
+    console.log("Pode-se cadastrar a peça.")
+} 
+else {
+    console.log("Peça com peso insuficiente.")
+}
+
+if(numeroPeca >=6) {
+
+    console.log("Número de peças suficientes.")
+}
+else {
+
+    console.log("Número de peças insuficientes.")
+}
+
+
+   
