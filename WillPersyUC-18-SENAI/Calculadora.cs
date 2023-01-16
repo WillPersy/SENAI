8c286c1f846fd002acf08c4ac2def356fe90d46f
Will Persy <willpersy@hotmail.com>
2023-01-16 16:48:52 -0300

    encontro-remoto

---
+++ WillPersyUC-18-SENAI/Calculadora.cs
@@ -0,0 +1,10 @@
+﻿namespace WillPersyUC18SENAI
+{
+    public class Calculadora
+    {
+        public double Somar(double pNum, double sNum)
+        {
+            return (pNum + sNum);
+        }
+    }
+}
